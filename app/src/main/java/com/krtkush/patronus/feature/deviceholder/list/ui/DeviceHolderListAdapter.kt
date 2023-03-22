a23da544d8d7df6f1aaf3614d8df9232ee30ef07
Kartikey Kushwaha <krtkush@users.noreply.github.com>
2023-03-22 15:27:08 +0100

    added rv adapter for list

--- app/src/main/java/com/krtkush/patronus/feature/deviceholder/list/ui/DeviceHolderListAdapter.kt
+++ app/src/main/java/com/krtkush/patronus/feature/deviceholder/list/ui/DeviceHolderListAdapter.kt
@@ -1,4 +1,29 @@
 package com.krtkush.patronus.feature.deviceholder.list.ui
 
-class DeviceHolderListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.krtkush.patronus.data.models.user.list.Customer
+import com.krtkush.patronus.databinding.UserListItemBinding
+
+class DeviceHolderListAdapter(private val userList: List<Customer>) : RecyclerView.Adapter<DeviceHolderListAdapter.UserItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserItemViewHolder {
+        val binding = UserListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserItemViewHolder, position: Int) {
+        val item = userList[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = userList.size
+
+    inner class UserItemViewHolder(private val binding : UserListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item : Customer) {
+
+        }
+    }
 }
